72392b0494317e5161bd2aebd0cc1cf57ebe09a3
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-20 21:47:12 +0900

    :recycle: OICインスタンス作成 ※一旦コメントアウト

---
+++ envs/oracle_integration_cloud.tf
@@ -0,0 +1,12 @@
+/************************************************************
+OIC インスタンス
+************************************************************/
+# resource "oci_integration_integration_instance" "developer" {
+#   compartment_id            = oci_identity_compartment.workload.id
+#   display_name              = "integration-instance"
+#   state                     = "ACTIVE"
+#   integration_instance_type = "STANDARDX"
+#   shape                     = "DEVELOPMENT"
+#   is_byol                   = false
+#   message_packs             = 1
+# }
