32f67a25d1d3360b99d94993aa6d4797eca5be09
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-20 21:46:14 +0900

    :recycle: 定義済みタグ追加

---
+++ envs/tags.tf
@@ -0,0 +1,76 @@
+/************************************************************
+Tag NameSpace (Compute)
+************************************************************/
+resource "oci_identity_tag_namespace" "compute" {
+  compartment_id = oci_identity_compartment.workload.id
+  name           = "Compute"
+  description    = "NameSpace For Compute"
+  is_retired     = false
+}
+
+resource "oci_identity_tag" "key_compute_system" {
+  tag_namespace_id = oci_identity_tag_namespace.compute.id
+  name             = "App"
+  description      = "Compute System Label"
+  is_cost_tracking = false
+  is_retired       = false
+  validator {
+    validator_type = "ENUM"
+    values         = ["web", "db"]
+  }
+}
+
+/************************************************************
+Tag NameSpace (Common)
+************************************************************/
+resource "oci_identity_tag_namespace" "common" {
+  compartment_id = oci_identity_compartment.workload.id
+  name           = "Common"
+  description    = "NameSpace for Common"
+  is_retired     = false
+}
+
+resource "oci_identity_tag" "key_system" {
+  tag_namespace_id = oci_identity_tag_namespace.common.id
+  name             = "System"
+  description      = "Defined System"
+  is_cost_tracking = true
+  is_retired       = false
+}
+
+resource "oci_identity_tag" "key_env" {
+  tag_namespace_id = oci_identity_tag_namespace.common.id
+  name             = "Env"
+  description      = "Defined Environment"
+  is_cost_tracking = true
+  is_retired       = false
+  validator {
+    validator_type = "ENUM"
+    values         = ["prd", "dev"]
+  }
+}
+
+resource "oci_identity_tag" "key_managedbyterraform" {
+  tag_namespace_id = oci_identity_tag_namespace.common.id
+  name             = "ManagedByTerraform"
+  description      = "Defined Managed By Terraform"
+  is_cost_tracking = true
+  is_retired       = false
+  validator {
+    validator_type = "ENUM"
+    values         = ["true", "false"]
+  }
+}
+
+/************************************************************
+Default Tag
+************************************************************/
+resource "oci_identity_tag_default" "key_system" {
+  compartment_id    = oci_identity_compartment.workload.id
+  tag_definition_id = oci_identity_tag.key_system.id
+  value             = "oci-oracle-integration-cloud-organize"
+  # TagがENUMでなく、値を固定しているためfalse
+  # リソース作成時に自動的に設定されるためfalse
+  # →trueにして、リソース作成時に強制明記させる必要がない
+  is_required = false
+}
